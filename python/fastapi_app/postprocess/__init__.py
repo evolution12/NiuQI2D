a6a0fd14baf3154ce1190de0dbc8664685463453
evolution12 <1159892474@qq.com>
2026-05-23 15:20:31 +0800

    Implement spritesheet builder

--- python/fastapi_app/postprocess/__init__.py
+++ python/fastapi_app/postprocess/__init__.py
@@ -6,14 +6,19 @@ from .frame_extractor import FrameExtractorStep
 from .quantize import QuantizeStep
 from .remove_bg import RemoveBackgroundStep
 from .resize import ResizeStep
+from .spritesheet import FrameMetadata, SpriteSheetConfig, SpriteSheetResult, build_sprite_sheet
 
 __all__ = [
     "CropCenterStep",
     "FrameExtractorStep",
+    "FrameMetadata",
     "PostProcessContext",
     "PostProcessLog",
     "PostProcessStep",
     "QuantizeStep",
     "RemoveBackgroundStep",
     "ResizeStep",
+    "SpriteSheetConfig",
+    "SpriteSheetResult",
+    "build_sprite_sheet",
 ]
